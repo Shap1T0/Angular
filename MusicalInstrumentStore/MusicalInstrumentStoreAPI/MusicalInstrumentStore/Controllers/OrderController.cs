3db427765e7ca961e6c299cf4fe13f38e81fbb0d
Egor.Nehaenko <nehaenkoegor@gmail.com>
2023-02-25 19:27:38 +0300

    Работа с Angular
    Пересобрал репозитории, проект на Angular имтация магазина музыкальных инструментов

---
+++ MusicalInstrumentStore/MusicalInstrumentStoreAPI/MusicalInstrumentStore/Controllers/OrderController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicalInstrumentStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+        private MusicalInstrumentStoreContext _context;
+        public OrderController(MusicalInstrumentStoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> Get()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> Get(Guid id)
+        {
+            Order order = await _context.Orders.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(order);
+        }
+
+        // POST api/users
+        [HttpPost]
+        public async Task<ActionResult<User>> Post(Order order)
+        {
+            if (order == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+            return Ok(order);
+        }
+
+        // PUT api/users/
+        [HttpPut]
+        public async Task<ActionResult<User>> Put(Order order)
+        {
+            if (order == null)
+            {
+                return BadRequest();
+            }
+            if (!_context.Orders.Any(x => x.Id == order.Id))
+            {
+                return NotFound();
+            }
+
+            _context.Update(order);
+            await _context.SaveChangesAsync();
+            return Ok(order);
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> Delete(Guid id)
+        {
+            Order order = _context.Orders.FirstOrDefault(x => x.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+            return Ok(order);
+        }
+    }
+}
---
+++ MusicalInstrumentStore/MusicalInstrumentStoreAPI/MusicalInstrumentStore/Controllers/UserController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicalInstrumentStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        MusicalInstrumentStoreContext db;
+        public UserController(MusicalInstrumentStoreContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            return await db.Users.ToListAsync();
+        }
+
+        #region Get 
+
+        /// <summary>
+        /// Получить контакта по Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("id/{id}")]
+        public async Task<ActionResult<User>> Get(Guid id)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(user);
+        }
+
+        /// <summary>
+        /// Получить контакта по Email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        [HttpGet("email/{email}")]
+        public async Task<ActionResult<User>> Get(string email)
+        {
+           User user = await db.Users.FirstOrDefaultAsync(x => x.Email == email);
+
+            //if (user == null)
+            //{
+            //    return NotFound();
+            //}
+
+            return new ObjectResult(user);
+        }
+
+        #endregion
+
+        // POST api/users
+        [HttpPost]
+        public async Task<ActionResult<User>> Post(User contact)
+        {
+            if (contact == null)
+            {
+                return BadRequest();
+            }
+
+            db.Users.Add(contact);
+            await db.SaveChangesAsync();
+            return Ok(contact);
+        }
+
+        // PUT api/users/
+        [HttpPut]
+        public async Task<ActionResult<User>> Put(User contact)
+        {
+            if (contact == null)
+            {
+                return BadRequest();
+            }
+            if (!db.Users.Any(x => x.Id == contact.Id))
+            {
+                return NotFound();
+            }
+
+            db.Update(contact);
+            await db.SaveChangesAsync();
+            return Ok(contact);
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> Delete(Guid id)
+        {
+            User contact = db.Users.FirstOrDefault(x => x.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            db.Users.Remove(contact);
+            await db.SaveChangesAsync();
+            return Ok(contact);
+        }
+    }
+}
